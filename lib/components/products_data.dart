83a7c221a670ad8710b0af764bfa57dcf9bda13a
unknown <joaovictor_gon@hotmail.com>
2024-04-26 23:06:24 -0300

    product data add

---
+++ lib/components/products_data.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ProductData {
+
+  late String id;
+  late String title;
+  late double price;
+  late List images;
+  late List size;
+
+
+  ProductData.DocumentSnapshot(DocumentSnapshot snapshot) {
+    id = snapshot.id;
+    title = snapshot.get('title');
+    price = snapshot.get('price');
+    images = snapshot.get('images');
+    size = snapshot.get('size');
+
+  }
+
+}
