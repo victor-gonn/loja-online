3e9839e56bac484ebedf2f70cf42e02ddfd11f69
unknown <joaovictor_gon@hotmail.com>
2024-06-20 12:34:44 -0300

    order screen add

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -6,6 +6,7 @@ import 'package:lojaonline2/components/cart_tile.dart';
 import 'package:lojaonline2/components/discount_cart.dart';
 import 'package:lojaonline2/components/ship_card.dart';
 import 'package:lojaonline2/screens/login_screen.dart';
+import 'package:lojaonline2/screens/order_screen.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class CartScreen extends StatefulWidget {
@@ -23,70 +24,94 @@ class _CartScreenState extends State<CartScreen> {
         title: Text('Meu Carrinho'),
         actions: [
           Container(
-            padding: EdgeInsets.only(right: 8),
-            alignment: Alignment.center,
-            child: ScopedModelDescendant<CartModel>(builder: (context, child, model) {
-              int p = model.product.length;
-              return Text('${p ?? 0} ${p == 1 ? 'item' : 'itens'}',
-              style: TextStyle(
-                fontSize: 17
-              ),);
-            },))
+              padding: EdgeInsets.only(right: 8),
+              alignment: Alignment.center,
+              child: ScopedModelDescendant<CartModel>(
+                builder: (context, child, model) {
+                  int p = model.product.length;
+                  return Text(
+                    '${p ?? 0} ${p == 1 ? 'item' : 'itens'}',
+                    style: TextStyle(fontSize: 17),
+                  );
+                },
+              ))
         ],
       ),
       body: ScopedModelDescendant<CartModel>(
         builder: (context, child, model) {
-          if(model.isLoading && UserModel.of(context).isLoggedIn()) {
-            return Center(child: CircularProgressIndicator(),);
-          } else if(!UserModel.of(context).isLoggedIn()) {
+          if (model.isLoading && UserModel.of(context).isLoggedIn()) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (!UserModel.of(context).isLoggedIn()) {
             return Container(
               padding: EdgeInsets.all(16),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Icon(Icons.remove_shopping_cart, size: 80, color: Colors.black,),
-                  SizedBox(height: 16,),
-                  Text('Faça login para visualizar seus produtos', 
-                  style: TextStyle(fontSize: 20),
-                  textAlign: TextAlign.center,),
-                  SizedBox(height: 16,),
+                  Icon(
+                    Icons.remove_shopping_cart,
+                    size: 80,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    'Faça login para visualizar seus produtos',
+                    style: TextStyle(fontSize: 20),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
                   ElevatedButton(
                     onPressed: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) =>LoginScreen())
-                      );
-                    }, 
-                    child: Text('Entrar', style: TextStyle(fontSize: 18),),)
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => LoginScreen()));
+                    },
+                    child: Text(
+                      'Entrar',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  )
                 ],
               ),
             );
-          // ignore: prefer_is_empty, unnecessary_null_comparison
-          } else if(model.product == null || model.product.length == 0) {
+            // ignore: prefer_is_empty, unnecessary_null_comparison
+          } else if (model.product == null || model.product.length == 0) {
             return Center(
-              child: Text('Nenhum produto adicionado', textAlign: TextAlign.center,),);
+              child: Text(
+                'Nenhum produto adicionado',
+                textAlign: TextAlign.center,
+              ),
+            );
           } else {
             return ListView(
               children: [
                 Column(
                   children: model.product.map((product) {
                     return CardTile(product);
-                  }
-                  ).toList(),
+                  }).toList(),
                 ),
                 DiscountCart(),
                 ShipCard(),
-                CartResume(() async{
+                CartResume(() async {
                   String? orderId = await model.finalOrder();
-                  if(orderId != null) {
-                    return print(orderId);
+                  if (orderId != null) {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: ((context) => OrderScreen(orderId)),
+                      ),
+                    );
                   }
                 })
               ],
             );
-            
           }
-        },),
+        },
+      ),
     );
   }
 }
---
+++ lib/screens/order_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class OrderScreen extends StatelessWidget {
+  const OrderScreen(this.orderId ,{super.key});
+
+  final String orderId; 
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+       title: Text('Pedido Realizado'),
+       centerTitle: true,),
+       body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.check,
+            color: Colors.blue,
+            size: 80,),
+            Text('Pedido realizado com sucesso',
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 18
+            ),),
+            Text('Codigo do pedido ${orderId}', style: TextStyle(
+              fontSize: 16
+            ),)
+          ],
+        ),
+       ),
+    );
+  }
+}
