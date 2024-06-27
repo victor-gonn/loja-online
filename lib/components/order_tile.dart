3487d2a861f515dce59c32751aa4c662a5d42bb3
unknown <joaovictor_gon@hotmail.com>
2024-06-27 00:54:10 -0300

    status de pedido add

--- lib/components/order_tile.dart
+++ lib/components/order_tile.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -20,14 +22,75 @@ class OrderTile extends StatelessWidget {
               child: CircularProgressIndicator(),
             );
           } else {
+
+            int status = snapshot.data?['status'];
             return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text('Código do pedido: ${snapshot.data!.id}',
-                style:  TextStyle(fontWeight: FontWeight.w500),)
+                style:  TextStyle(fontWeight: FontWeight.w500),
+                ),
+                SizedBox(height: 4,),
+                Text(_buildProductsText(snapshot.data!)),
+                SizedBox(height: 4,),
+                Text('Status do pedido:',
+                style:  TextStyle(fontWeight: FontWeight.w500),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _buildStatus('1', 'Preparação', status, 1),
+                    Container(width: 40, height: 1, color: Colors.grey,),
+                    _buildStatus('2', 'Transporte', status, 2),
+                    Container(width: 40, height: 1, color: Colors.grey,),
+                    _buildStatus('3', 'Entrega', status, 3)
+                  ],
+                )
+                
               ],
             );
           }
          }),),
     );
+  }
+
+  String _buildProductsText(DocumentSnapshot snapshot) {
+    String text = 'Descrição:\n';
+   
+      for(LinkedHashMap p in snapshot['products']) {
+      text += '${p['quantity']} x ${p['product'] ['price']} \n';
+    
+    }
+    text += 'Total: R\$ ${snapshot['totalPrice']}';
+    return text;
+    
+  }
+
+  Widget _buildStatus(String title, String subtitle, int status, int thisStatus) {
+    Widget child;
+    Color backColor;
+
+    if(status < thisStatus) {
+      backColor = Colors.grey;
+      child = Text(title, style: TextStyle(color: Colors.white),);
+    } else if( status == thisStatus) {
+      backColor = Colors.blue;
+      child = Text(title, style: TextStyle(color: Colors.white),);
+    } else {
+      backColor = Colors.green;
+      child = Icon(Icons.check, color: Colors.white,);
+    }
+
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 20,
+          backgroundColor: backColor,
+          child: child,
+        ),
+        Text(subtitle)
+      ],
+    );
+
   }
 }
